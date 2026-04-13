5dc36e2f90eb7635162b28717328de682dd7b523
Sanya Agarwal <agarwalsanya90@gmail.com>
2026-04-13 13:33:03 +0530

    Update vector_db.py

--- vector_db.py
+++ vector_db.py
@@ -3,14 +3,14 @@ from qdrant_client.models import VectorParams, Distance, PointStruct
 
 
 class QdrantStorage:
-    def __init__(self, url="http://localhost:6333", collection="docs", dim=3072):
-        self.client = QdrantClient(url=url, timeout=30)
-        self.collection = collection
-        if not self.client.collection_exists(self.collection):
-            self.client.create_collection(
-                collection_name=self.collection,
-                vectors_config=VectorParams(size=dim, distance=Distance.COSINE),
-            )
+     def __init__(self, collection="docs", dim=3072):
+    self.client = QdrantClient(path="./qdrant_storage")
+    self.collection = collection
+    if not self.client.collection_exists(self.collection):
+        self.client.create_collection(
+            collection_name=self.collection,
+            vectors_config=VectorParams(size=dim, distance=Distance.COSINE),
+        )
 
     def upsert(self, ids, vectors, payloads):
         points = [PointStruct(id=ids[i], vector=vectors[i], payload=payloads[i]) for i in range(len(ids))]
